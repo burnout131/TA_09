5310de1ea94ff1d7ed59e0b9b9057deca0145d94
burnout131 <marcrg07@gmail.com>
2023-03-15 19:21:45 +0100

    Cambios en Personas

--- src/ejercicio5/Personas.java
+++ src/ejercicio5/Personas.java
@@ -4,7 +4,36 @@ public abstract class Personas {
 	public String nombre;
 	public int edad;
 	public Genero genero;
-	
-	
+
+	public Personas(String nombre, int edad, Genero genero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.genero = genero;
+	}
+
 	public abstract boolean disponibilidad();
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
 }
