5b59b819bb64f532ba11e73dd9e215808b804abe
Paula Sousa <paulasousa137@gmail.com>
2023-03-15 18:30:40 +0100

    Aula metodo darClase

--- src/ejercicio5/Aula.java
+++ src/ejercicio5/Aula.java
@@ -1,5 +1,36 @@
 package ejercicio5;
 
+import java.util.ArrayList;
+
 public class Aula {
+	private int idAula;
+	private int maxEstudiantes;
+	Materia materia;
+
+	public boolean darClase(ArrayList<Profesor[]> arrayProfesor, ArrayList<Estudiante[]> arrayEstudiante) {
+
+		for (Estudiante[] estudiantes : arrayEstudiante) {
+	        for (Estudiante estudiante : estudiantes) {
+	            if (disponibilidad(estudiante)) {
+	                for (Profesor[] profesores : arrayProfesor) {
+	                    for (Profesor profesor : profesores) {
+	                        if (profesor.materia == this.materia) {
+	                            if (disponibilidad(profesor)) {
+	                                return true;
+	                            }
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    return false;
+
+	}
+
+	public String mostrarAprobados(Estudiante[] arrayEstudiantes) {
+
+		return arrayEstudiantes[1].toString();
+	}
 
 }
