b3544483b7ae5c91df599cbe667c41c0fceaf84b
Paula Sousa <paulasousa137@gmail.com>
2023-03-16 10:33:38 +0100

    Metodos tiene raiz + calcular

--- src/ejercicio4/Raices.java
+++ src/ejercicio4/Raices.java
@@ -1,19 +1,35 @@
 package ejercicio4;
 
 public class Raices {
-	
+
 	private double a;
 	private double b;
 	private double c;
-	
-	
+
 	public Raices(double a, double b, double c) {
 		super();
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
-	
-	
+
+	public boolean tieneRaiz(double discriminante) {
+		if (discriminante == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void calcular(double discriminante) {
+		if (discriminante > 0) {
+			System.out.println("La ecuación tiene 2 soluciones.");
+		} else if (discriminante == 0) {
+			System.out.println("La ecuación tiene 1 solución.");
+		} else {
+			System.out.println("La ecuación no tiene solución.");
+		}
+
+	}
 
 }
