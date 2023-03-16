d100787a4bdecfa23545427a96d1028ff91684ce
Paula Sousa <paulasousa137@gmail.com>
2023-03-16 11:38:07 +0100

    Peliculas

--- src/ejercicio6/MainApp.java
+++ src/ejercicio6/MainApp.java
@@ -1,5 +1,9 @@
 package ejercicio6;
 
 public class MainApp {
+	
+	Peliculas pelicula1 = new Peliculas("The Shawshank Redemption", 142, 12, "Frank Darabont");
+	Peliculas pelicula2 = new Peliculas("The Godfather", 177, 16, "Ford Coppola");
+	Peliculas pelicula3 = new Peliculas("The Dark Knight", 152, 12, "Christoper Nolan");
 
 }
