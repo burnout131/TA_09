9c0296497047f90cfcbf0e0ce24ab7d33fd053ba
Paula Sousa <paulasousa137@gmail.com>
2023-03-16 12:19:10 +0100

    Espectadores

--- src/ejercicio6/MainApp.java
+++ src/ejercicio6/MainApp.java
@@ -5,5 +5,17 @@ public class MainApp {
 	Peliculas pelicula1 = new Peliculas("The Shawshank Redemption", 142, 12, "Frank Darabont");
 	Peliculas pelicula2 = new Peliculas("The Godfather", 177, 16, "Ford Coppola");
 	Peliculas pelicula3 = new Peliculas("The Dark Knight", 152, 12, "Christoper Nolan");
+	
+	
+	Espectador espectador1 = new Espectador("Sandra Gomez", 14, 10.0);
+	Espectador espectador2 = new Espectador("Ricando Saez", 12, 15.0);
+	Espectador espectador3 = new Espectador("Andreu Creus", 10, 10.0);
+	Espectador espectador4 = new Espectador("Carlos Lopez", 18, 6.0);
+	Espectador espectador5 = new Espectador("Enrique Vasquez", 14, 20.0);
+	Espectador espectador6 = new Espectador("Jana Smith", 16, 50.0);
+	Espectador espectador7 = new Espectador("Lorena Martinez", 20, 100.0);
+	Espectador espectador8 = new Espectador("Anna Bonin", 11, 8.0);
+	Espectador espectador9 = new Espectador("Joan Sousa", 14, 5.0);
+	Espectador espectador10 = new Espectador("Paula Jimenez", 17, 18.0);
 
 }
