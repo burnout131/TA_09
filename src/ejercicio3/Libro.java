f4e7ba97b9b6cefc25694509206529bf3f427060
burnout131 <marcrg07@gmail.com>
2023-03-16 09:54:27 +0100

    Se crea la clase Libro

---
+++ src/ejercicio3/Libro.java
@@ -0,0 +1,55 @@
+package ejercicio3;
+
+public class Libro {
+	private String ISBN;
+	private String titulo;
+	private String autor;
+	private int numeroPaginas;
+	
+	public Libro(String ISBN, String titulo, String autor, int numeroPaginas) {
+		this.ISBN = ISBN;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public void setNumeroPaginas(int numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	@Override
+	public String toString() {
+		return "Libro con ISBN=" + ISBN + ", autor=" + autor + ", numeroPaginas=" + numeroPaginas + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
+}
