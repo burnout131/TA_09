d2a389282326477abd4733c9136ae9394db14b88
Roger <rogergibert8@gmail.com>
2023-03-19 11:30:37 +0100

    Asignacion de asientos en clase Cine

--- src/ejercicio6/Cine.java
+++ src/ejercicio6/Cine.java
@@ -1,23 +1,79 @@
 package ejercicio6;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Cine {
+	private final int FILAS = 8;
+	private final int COLUMNAS = 9;
+
 	private Peliculas pelicula;
 	private double precio;
-	
+	// Array de FILAS y COLUMNAS que simulan los asientos, se le asignaran espectadores
+	private Espectador[][] EspectadoresSala = new Espectador[FILAS][COLUMNAS];
+
 	public Cine(Peliculas pelicula, double precio) {
 		this.pelicula = pelicula;
 		this.precio = precio;
+		VaciarAsientos();
 	}
-	
+
+	// Asigna todos los asientos vacios (null)
+	public void VaciarAsientos() {
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				EspectadoresSala[i][j] = null;
+			}
+		}
+	}
+
+	// Indica si el asiento tiene un Espectador asignado o si esta vacio (null)
+	public boolean AsientoOcupado(int fila, int columna) {
+		return EspectadoresSala[fila][columna] != null;
+	}
+
+	/*
+	 * Asigna un asiento vacio a Espectador. Devuelve un boolean indicando si se ha
+	 * podido asignar un asiento
+	 */
+	public boolean AssignarAsiento(Espectador espectador) {
+		boolean asientoAsignado = false;
+		Random rand = new Random();
+		ArrayList<Integer> asientosVacios = new ArrayList<Integer>();
+
+		// Añade los asientos vacios a una lista
+		for (int i = 0; i < FILAS; i++) {
+			for (int j = 0; j < COLUMNAS; j++) {
+				if (!AsientoOcupado(i, j)) {
+					asientosVacios.add(i * COLUMNAS + j);
+				}
+			}
+		}
+
+		// Comprueba si hay asientos vacios
+		if (!asientosVacios.isEmpty()) {
+			// Selecciona un asiento aleatorio
+			int posicion = rand.nextInt(asientosVacios.size());
+
+			// Asigna el asiento al Espectador
+			EspectadoresSala[posicion / COLUMNAS][posicion % COLUMNAS] = espectador;
+			asientoAsignado = true;
+		}
+		return asientoAsignado;
+	}
+
 	public Peliculas getPelicula() {
 		return pelicula;
 	}
+
 	public void setPelicula(Peliculas pelicula) {
 		this.pelicula = pelicula;
 	}
+
 	public double getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
@@ -26,5 +82,5 @@ public class Cine {
 	public String toString() {
 		return "Cine [pelicula=" + pelicula + ", precio=" + precio + ", toString()=" + super.toString() + "]";
 	}
-	
+
 }
